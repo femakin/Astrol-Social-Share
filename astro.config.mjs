4db51f4efa6854c9dfb3928f1da639377394da3c
femi <oluwafemi.akinyemi@datamellon.com>
2023-10-23 14:22:27 +0100

    Added Netlify Adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 import node from "@astrojs/node";
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,4 +11,5 @@ export default defineConfig({
   adapter: node({
     mode: "standalone",
   }),
+  adapter: netlify(),
 });
